208e1fdb579b4177fcdbf82a3f7c45b504f0dde8
zp <2387787367@qq.com>
2019-09-01 13:26:17 +0800

    sdd

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
-
+zzww
+from django.views import defaults as default_views
+from django.views import defaults as default_views
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
